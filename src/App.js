860f982aa5a1a5d4f03dd97231c37112dd6a199e
chsophie <saumon14c@soph-MacBook-Air.local>
2025-03-19 18:15:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import humidity from './assets/humidity.png';
 import wind from './assets/wind.png';
 import sunrise from './assets/sunrise.png';
 import sunset from './assets/sunset.png';
+import warmth from './assets/warm.png';
+import cold from './assets/low-temperature.png';
 
 const App = () => {
     const [city, setCity] = useState('');
@@ -26,23 +28,10 @@ const App = () => {
         }
     };
 
-    /* const getImage = () => {
-      if(!weatherData) return 'url(../assets/cloudy.jpg)'; // default 
-
-      const description = weatherData.weather[0].main.toLowerCase();
-      switch(description){
-        case 'Clouds':
-          return 'url(../assets/cloudy.jpg)';
-          default:
-            return 'url(../assets/cloudy.jpg)'; 
-      }
-    }
-    */
-
     const getCountryName = (countryCode) => {
       const formatter = new Intl.DisplayNames(['en'], { type: 'region' });
       return formatter.of(countryCode);
-  };
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -65,40 +54,35 @@ const App = () => {
         {error && <p>{error}</p>}
         {weatherData && (
           <div className='container'>
-                <div className='upper-data'>
-
-                  <div className='weather-data'>
+              <div className='upper-data'>
+                <div className='weather-data'>
                   <h3 className='location'>{weatherData.name + ", " + getCountryName(weatherData.sys.country)}</h3>
-                  <span className='temperature'>Temperature: {Math.round(weatherData.main.temp)}°C</span>
-                  <div className='maxmin'>
-                  <span>H: {Math.round(weatherData.main.temp_max)}° </span>
-                  <span>L: {Math.round(weatherData.main.temp_min)}° </span>
-                  </div>
-                  <span>{(weatherData.weather[0].main)}</span>
-                  </div>
-
+                  <span className='temperature'>{Math.round(weatherData.main.temp)}°C</span>
+                  <span className='description'>{(weatherData.weather[0].main)}</span>
                 </div>
-                <div className='lower-data'>
+              </div>
+              <div className='lower-data'>
                   <div className='upper-block'>
                     <img title='humidity-logo' alt='humidity-logo' width="9%" src={humidity} />
                     <p className='humidity'>Humidity: {weatherData.main.humidity}%</p>
                     <img title='wind-logo' alt='wind-logo' width="9%" src={wind} />
                     <p className='wind'>Wind: {Math.round(weatherData.wind.speed * 3.6)} km/h</p>
                   </div>
-                  <div className='lower-block'>
+                  <div className='middle-block'>
+                    <img title='warmth-logo' alt='warmth-logo' width="9%" src={warmth} />
+                    <span className='high'>Max: {Math.round(weatherData.main.temp_max)}°C </span>
+                    <img title='cold-logo' alt='cold-logo' width="9%" src={cold} />
+                    <span className='low'>Min: {Math.round(weatherData.main.temp_min)}°C </span>
+                  </div>
+                  <div className='lower-block'>                
                     {/* sun time * 1000 to miliseconds */}
                     <img title='sunrise-logo' alt='sunrise-logo' width="10%" src={sunrise} />
                     <p className='sunrise'>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })}</p>
                     <img title='sunset-logo' alt='sunset-logo' width="10%" src={sunset} />
                     <p className='sunset'>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })}</p>
-                  </div>
-                
-                {/* hourly forecast */}
-                {/* 5 days forecast */}
-                {/* moon ? */}
+                  </div>                
                 </div>
-            </div>
-            
+            </div>          
         )}
     </div>
     );
